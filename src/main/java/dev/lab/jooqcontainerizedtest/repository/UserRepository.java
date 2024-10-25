f41e63f9aa9dfa10552f919d4f1a66eb054fb6c6
keemsora <itsyoursora@gmail.com>
2024-10-25 22:33:25 +0900

    feat: UserRepository 구현

--- src/main/java/dev/lab/jooqcontainerizedtest/repository/UserRepository.java
+++ src/main/java/dev/lab/jooqcontainerizedtest/repository/UserRepository.java
@@ -1,18 +1,38 @@
 package dev.lab.jooqcontainerizedtest.repository;
 
-import static java.util.stream.Collectors.*;
+import static org.jooq.Records.*;
+import static org.jooq.generated.tables.Users.*;
+import static org.yaml.snakeyaml.nodes.NodeId.*;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.jooq.DSLContext;
-import org.springframework.stereotype.Repository;
+import org.jooq.Records;
 
-@Repository
+import dev.lab.jooqcontainerizedtest.domain.User;
+
 public class UserRepository {
 
 	private final DSLContext dslContext;
 
 	public UserRepository(DSLContext dslContext) {
 		this.dslContext = dslContext;
+	}
+
+	public User createUser(User user) {
+		return this.dslContext.insertInto(USERS)
+			.set(USERS.NAME, user.name())
+			.set(USERS.EMAIL, user.email())
+			.set(USERS.REG_DATE, LocalDateTime.now())
+			.returningResult(USERS.ID, USERS.NAME, USERS.EMAIL)
+			.fetchOne(mapping(User::new));
+	}
+
+	public Optional<User> getUserByEmail(String email) {
+		return this.dslContext.select(USERS.ID, USERS.NAME, USERS.EMAIL)
+			.from(USERS)
+			.where(USERS.EMAIL.equalIgnoreCase(email))
+			.fetchOptional(mapping(User::new));
 	}
 }
