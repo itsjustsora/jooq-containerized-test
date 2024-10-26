21c38030efe9800992349578dfccef4907f7d9c5
keemsora <itsyoursora@gmail.com>
2024-10-26 23:42:37 +0900

    feat: 테스트 sql 스크립트 작성

---
+++ src/test/resources/test-data.sql
@@ -0,0 +1,5 @@
+DELETE FROM users;
+
+INSERT INTO users(id, name, email) VALUES
+                                       (1, 'Siva', 'siva@gmail.com'),
+                                       (2, 'Oleg', 'oleg@gmail.com');
